742cb30fbf08c6919f0f57724c0caa22989fa85b
roronoaharish <harishchetty1997@gmail.com>
2017-03-20 20:07:09 +0530

    Update script.js

--- jason/js/script.js
+++ jason/js/script.js
@@ -3,17 +3,39 @@ document.addEventListener("DOMContentLoaded",
   function (event) {
     
     // Unobtrusive event binding
-    document.querySelector("button")
+    document.querySelector("#button1")
       .addEventListener("click", function () {
         
         // Call server to get the name
         $ajaxUtils
           .sendGetRequest("data/name.json", 
             function (res) {
-              var message = "Addition of :"
-                res.firstName + "and" + res.lastName +"is";
-          
+              var message = "Addition of :";
+                 message +=  res.firstName + "and" + res.lastName +"is";       
            var message1 = res.firstName+res.lastName;
+              if (res.addition) {
+             
+                message +=  message1;
+              }
+              else {
+                message += " not found!";
+              }
+              
+
+              document.querySelector("#content")
+                .innerHTML = "<h2>" + message + "</h2>";
+            });
+      });
+  document.querySelector("#button2")
+      .addEventListener("click", function () {
+        
+        // Call server to get the name
+        $ajaxUtils
+          .sendGetRequest("data/name.json", 
+            function (res) {
+              var message = "Subtraction of :";
+                 message +=  res.firstName + "and" + res.lastName +"is";       
+           var message1 = res.firstName-res.lastName;
               if (res.addition) {
              
                 message +=  message1;
