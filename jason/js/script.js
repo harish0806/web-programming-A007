92ff76ab1656333d6f7e62b8ec5f3f7da6772846
roronoaharish <harishchetty1997@gmail.com>
2017-03-21 15:13:40 +0530

    Update script.js

--- jason/js/script.js
+++ jason/js/script.js
@@ -15,12 +15,12 @@ document.addEventListener("DOMContentLoaded",
                  
               if ( temp === res.student1) {
              
-                var temp2= res.course1+": "+res.marks11;
-                var temp1= res.course2+": "+res.marks12;
+                var temp2= res.course1+": "+res.marks11
+                var temp1= res.course2+": "+res.marks12
               }
               else if (temp === res.student2) {
-                var temp2= res.course1+": "+res.marks21;
-                var temp1= res.course2+": "+res.marks22;
+                var temp2= res.course1+": "+res.marks21
+                var temp1= res.course2+": "+res.marks22
               }
           else {
             var temp1 = "no such student";
