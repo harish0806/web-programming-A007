e1286bced1a043fbc9e1d419f1e3facec4d9bb80
roronoaharish <harishchetty1997@gmail.com>
2017-03-21 15:03:26 +0530

    Update script.js

--- jason/js/script.js
+++ jason/js/script.js
@@ -29,7 +29,7 @@ document.addEventListener("DOMContentLoaded",
 
               document.querySelector("#content")
                 .innerHTML = "<h2>" + temp2+ "</h2>";
-          document.querySelector("#content")
+          document.querySelector("#content1")
                 .innerHTML = "<h2>" + temp1+ "</h2>";
             });
       });
